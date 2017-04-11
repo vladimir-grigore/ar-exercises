0e1061824dea615ed69fc2c351c7219b8d9405b1
Vladimir Grigore <vladimir.grigore@gmail.com>
2017-04-11 23:45:22 +0000

    Create before_destroy validation on stores with employees

--- lib/store.rb
+++ lib/store.rb
@@ -3,6 +3,7 @@ class Store < ActiveRecord::Base
   validates :name, presence: true, length: { minimum: 3 }
   validates_numericality_of :annual_revenue, greater_than: 0, only_integer: true
   validate :apparel
+  before_destroy :check_if_has_employees
 
   private
 
@@ -11,6 +12,10 @@ class Store < ActiveRecord::Base
       errors.add(:mens_apparel, "must either sell mens or womens apparrel")
       errors.add(:womens_apparel, "must either sell mens or womens apparrel")
     end
+  end
+
+  def check_if_has_employees
+    false if Employee.find_by(store_id: self.id)
   end
   
 end
